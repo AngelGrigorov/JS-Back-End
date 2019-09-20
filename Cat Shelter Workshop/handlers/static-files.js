935359d58fa5afc01ea6f99c636e947709ad9ddb
AngelGrigorov <damage9797@gmail.com>
2019-09-20 19:32:58 +0300

    more fixes

--- Cat Shelter Workshop/handlers/static-files.js
+++ Cat Shelter Workshop/handlers/static-files.js
@@ -4,7 +4,7 @@ function getContentType(url){
     if(url.endsWith('css')){
         return 'text/css';
     }else if(url.endsWith('html')){
-        return 'text.html';
+        return 'text/html';
     }else if(url.endsWith('png')){
     return 'image/png';
     }else if(url.endsWith('js')){
